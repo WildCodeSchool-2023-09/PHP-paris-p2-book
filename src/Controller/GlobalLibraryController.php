035b058569fd8c8bbd1bbb538076fb4d0eb8947d
Eddy <125514698+KarlRow@users.noreply.github.com>
2023-11-06 15:02:55 +0100

    first draft of the global library

---
+++ src/Controller/GlobalLibraryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+class GlobalLibraryController extends AbstractController
+{
+    /**
+     * Display home page
+     */
+    public function GlobalLibrary(): string
+    {
+        return $this->twig->render('GlobalLibrary/global_library.html.twig');
+    }
+}
