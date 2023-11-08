7a30ca7c2cee1fa4df7f6c3cd2b40a56377f3a1b
nicolasalibert <contact@nicolasalibert.me>
2023-11-08 16:49:21 +0100

    removed console logs in js script

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -2,16 +2,12 @@ const userMenu = document.querySelector("#user-menu");
 const userBtn = document.querySelector("#btn-user");
 const userBtnClose = document.querySelector("#btn-user-close");
 
-console.log("js script working");
-
 userBtn.addEventListener('click', function() {
     userMenu.style.display = "block";
     userBtn.style.visibility = "hidden";
-    console.log("opened user menu");
 });
 
 userBtnClose.addEventListener('click', function() {
     userMenu.style.display = "none";
     userBtn.style.visibility = "visible";
-    console.log("closed user menu");
 });
