ee6dd58e1c73902bf5cc3c42713d8066c32ea05d
Eddy <125514698+KarlRow@users.noreply.github.com>
2023-11-14 18:10:39 +0100

    adding mylibrary, read, wishlist icons + text. Update of css and responsive desktop, unfinished

--- src/Model/GlobalLibraryManager.php
+++ src/Model/GlobalLibraryManager.php
@@ -4,19 +4,16 @@ namespace App\Model;
 
 class GlobalLibraryManager extends AbstractManager
 {
-    public const TABLE = 'book';
-
-    public const AUTHOR = 'author';
-
-    public const BOOK_EDITOR = 'book_editor';
+    // public const TABLE = 'book';
+    // public const AUTHOR = 'author';
+    // public const BOOK_EDITOR = 'book_editor';
 
     public function selectAll(string $orderBy = '', string $direction = 'ASC'): array
     {
-        $query = 'SELECT ' . static::TABLE . '.title, ' . static::TABLE . '.written_at, '
-        . static::AUTHOR . '.firstname, ' . static::AUTHOR . '.lastname, ' . static::BOOK_EDITOR . '.cover 
-        FROM ' . static::TABLE . ' 
-        JOIN ' . static::AUTHOR . ' ON ' . static::TABLE . '.id = ' . static::AUTHOR . '.id 
-        JOIN ' . static::BOOK_EDITOR . ' ON ' . static::TABLE . '.id = ' . static::BOOK_EDITOR . '.id';
+        $query = 'SELECT book.title, book.written_at, author.firstname, author.lastname, book_editor.cover 
+        FROM book 
+        JOIN author ON book.id=author.id
+        JOIN book_editor ON book.id=book_editor.id';
         if ($orderBy) {
             $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
         }
