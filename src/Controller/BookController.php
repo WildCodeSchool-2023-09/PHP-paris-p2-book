8ecbbd8eb39fce25b633d108593e3b05ebf871aa
2z75 <zinedine.chader@mail.com>
2023-11-16 09:23:34 +0100

    S03US03 Page of One Book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,9 +8,10 @@ class BookController extends AbstractController
 {
     public function show(int $id): string
     {
-        $BookEditionManager = new BookEditionManager();
-        $book = $BookEditionManager->selectOneById($id);
-
+        $bookEditionManager = new BookEditionManager();
+        $book = $bookEditionManager->selectOneById($id);
+        
         return $this->twig->render('Book/show.html.twig', ['book' => $book]);
     }
+    
 }
