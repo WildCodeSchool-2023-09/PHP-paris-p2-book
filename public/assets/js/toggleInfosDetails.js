b35426b0a00b9ddf133f263168394d23fd122b5a
2z75 <zinedine.chader@mail.com>
2023-11-21 10:31:28 +0100

    chakal terminus quel bail

--- public/assets/js/toggleInfosDetails.js
+++ public/assets/js/toggleInfosDetails.js
@@ -1,14 +1,24 @@
 const synopsis = document.getElementById("synopsis");
 const details = document.getElementById("details");
+const review = document.getElementById("review")
 const synopsisTitle = document.getElementById("synopsisTitle");
 const detailTitle = document.getElementById("detailTitle");
+const reviewTitle = document.getElementById("reviewTitle");
 
 synopsisTitle.addEventListener("click", () => {
     synopsis.style.display="block";
     details.style.display="none";
+    review.style.display="none";
 });
 
 detailTitle.addEventListener("click", () => {
     details.style.display="block";
+    synopsis.style.display="none";
+    review.style.display="none";
+});
+
+reviewTitle.addEventListener("click", () => {
+    review.style.display="block";
+    details.style.display="none";
     synopsis.style.display="none";
 });
